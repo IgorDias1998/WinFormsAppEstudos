9a3bfa36d08a7e6ad45f23a8fa8ecc22a19c8d73
IgorDias1998 <igordiass1998@gmail.com>
2024-04-12 16:01:35 -0300

    Iniciando projeto Windows Forms

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WinFormsAppEstudos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            button1 = new Button();
+            label6 = new Label();
+            textBox5 = new TextBox();
+            button2 = new Button();
+            listView1 = new ListView();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(10, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Produto";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(10, 71);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(231, 27);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(303, 48);
+            label2.Name = "label2";
+            label2.Size = new Size(138, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Código do produto";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(303, 71);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(138, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(10, 128);
+            label3.Name = "label3";
+            label3.Size = new Size(74, 20);
+            label3.TabIndex = 4;
+            label3.Text = "Descrição";
+            label3.Click += label3_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(10, 151);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(431, 27);
+            textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(10, 202);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 20);
+            label4.TabIndex = 6;
+            label4.Text = "Preço";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(52, 225);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(20, 232);
+            label5.Name = "label5";
+            label5.Size = new Size(26, 20);
+            label5.TabIndex = 8;
+            label5.Text = "R$";
+            label5.Click += label5_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(289, 225);
+            button1.Name = "button1";
+            button1.Size = new Size(152, 29);
+            button1.TabIndex = 9;
+            button1.Text = "Salvar Produto";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(489, 48);
+            label6.Name = "label6";
+            label6.Size = new Size(110, 20);
+            label6.TabIndex = 10;
+            label6.Text = "Buscar produto";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(489, 71);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(219, 27);
+            textBox5.TabIndex = 11;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(723, 71);
+            button2.Name = "button2";
+            button2.Size = new Size(101, 27);
+            button2.TabIndex = 12;
+            button2.Text = "Buscar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(489, 128);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(335, 121);
+            listView1.TabIndex = 13;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(836, 346);
+            Controls.Add(listView1);
+            Controls.Add(button2);
+            Controls.Add(textBox5);
+            Controls.Add(label6);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label5;
+        private Button button1;
+        private Label label6;
+        private TextBox textBox5;
+        private Button button2;
+        private ListView listView1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+using MySql.Data.MySqlClient;
+using System.Net.Mail;
+using WinFormsAppEstudos.SecurityBanco;
+
+namespace WinFormsAppEstudos
+{
+    public partial class Form1 : Form
+    {
+        Password senha = new Password();
+        MySqlConnection Conexao;
+        private string data_source;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            data_source = $"datasource=localhost;username=root;password={senha.PasswordStorage()};database=Venda_Produto";
+
+
+            listView1.View = View.Details;
+            listView1.LabelEdit = true;
+            listView1.AllowColumnReorder = true;
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+
+            listView1.Columns.Add("Nome", 50, HorizontalAlignment.Left);
+            listView1.Columns.Add("Código", 50, HorizontalAlignment.Left);
+            listView1.Columns.Add("Descrição", 50, HorizontalAlignment.Left);
+            listView1.Columns.Add("Preço", 50, HorizontalAlignment.Left);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Conexao = new MySqlConnection(data_source);
+
+                string sql = "INSERT INTO Produto (pro_codigo, pro_nome, pro_descricao, pro_preco)" +
+                    "VALUES('" + textBox2.Text + "', '" + textBox1.Text + "', '" + textBox3.Text + "', '" + textBox4.Text + "')";
+
+                MySqlCommand comando = new MySqlCommand(sql, Conexao);
+
+                Conexao.Open();
+
+                comando.ExecuteReader();
+
+                MessageBox.Show("Produto cadastrado com sucesso");
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string q = "'%" + textBox5.Text + "%'";
+
+                Conexao = new MySqlConnection(data_source);
+
+                string sql = "SELECT * " +
+                            "FROM Produto " +
+                            "WHERE pro_nome LIKE " + q;
+
+                Conexao.Open();
+
+                MySqlCommand comando = new MySqlCommand(sql, Conexao);
+
+                MySqlDataReader reader = comando.ExecuteReader();
+                
+                listView1.Items.Clear();
+
+                while(reader.Read())
+                {
+                    string[] row =
+                    {
+                        reader.GetInt32(0).ToString(),
+                        reader.GetString(1), 
+                        reader.GetString(2), 
+                        reader.GetDouble(3).ToString()
+                    };
+
+                    var linha_listView = new ListViewItem(row);
+
+                    listView1.Items.Add(linha_listView);
+                }
+
+                MessageBox.Show("Produto cadastrado com sucesso");
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+    }
+}
