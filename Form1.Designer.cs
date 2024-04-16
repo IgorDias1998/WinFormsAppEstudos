e12018253fbf840daab2405035118ea7b1aa2104
IgorDias1998 <igordiass1998@gmail.com>
2024-04-16 15:28:12 -0300

    Alterações

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             label1 = new Label();
             textBox1 = new TextBox();
             label2 = new Label();
@@ -42,136 +43,171 @@
             textBox5 = new TextBox();
             button2 = new Button();
             listView1 = new ListView();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            menuStrip1 = new MenuStrip();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(10, 48);
+            label1.Location = new Point(11, 45);
             label1.Name = "label1";
-            label1.Size = new Size(62, 20);
+            label1.Size = new Size(63, 19);
             label1.TabIndex = 0;
             label1.Text = "Produto";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(10, 71);
+            textBox1.Location = new Point(11, 68);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(231, 27);
+            textBox1.Size = new Size(259, 26);
             textBox1.TabIndex = 1;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(303, 48);
+            label2.Location = new Point(341, 45);
             label2.Name = "label2";
-            label2.Size = new Size(138, 20);
+            label2.Size = new Size(137, 19);
             label2.TabIndex = 2;
             label2.Text = "Código do produto";
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(303, 71);
+            textBox2.Location = new Point(341, 68);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(138, 27);
+            textBox2.Size = new Size(155, 26);
             textBox2.TabIndex = 3;
             textBox2.TextChanged += textBox2_TextChanged;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(10, 128);
+            label3.Location = new Point(11, 121);
             label3.Name = "label3";
-            label3.Size = new Size(74, 20);
+            label3.Size = new Size(78, 19);
             label3.TabIndex = 4;
             label3.Text = "Descrição";
             label3.Click += label3_Click;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(10, 151);
+            textBox3.Location = new Point(11, 144);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(431, 27);
+            textBox3.Size = new Size(484, 26);
             textBox3.TabIndex = 5;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(10, 202);
+            label4.Location = new Point(56, 199);
             label4.Name = "label4";
-            label4.Size = new Size(46, 20);
+            label4.Size = new Size(47, 19);
             label4.TabIndex = 6;
             label4.Text = "Preço";
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(52, 225);
+            textBox4.Location = new Point(56, 221);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(125, 27);
+            textBox4.Size = new Size(140, 26);
             textBox4.TabIndex = 7;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(20, 232);
+            label5.Location = new Point(22, 228);
             label5.Name = "label5";
-            label5.Size = new Size(26, 20);
+            label5.Size = new Size(28, 19);
             label5.TabIndex = 8;
             label5.Text = "R$";
             label5.Click += label5_Click;
             // 
             // button1
             // 
-            button1.Location = new Point(289, 225);
+            button1.BackColor = Color.SteelBlue;
+            button1.Cursor = Cursors.Hand;
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(325, 213);
             button1.Name = "button1";
-            button1.Size = new Size(152, 29);
+            button1.Size = new Size(171, 34);
             button1.TabIndex = 9;
             button1.Text = "Salvar Produto";
-            button1.UseVisualStyleBackColor = true;
+            button1.TextImageRelation = TextImageRelation.ImageAboveText;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(489, 48);
+            label6.Location = new Point(550, 54);
             label6.Name = "label6";
-            label6.Size = new Size(110, 20);
+            label6.Size = new Size(114, 19);
             label6.TabIndex = 10;
             label6.Text = "Buscar produto";
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(489, 71);
+            textBox5.Location = new Point(550, 76);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(286, 27);
+            textBox5.Size = new Size(321, 26);
             textBox5.TabIndex = 11;
             // 
             // button2
             // 
-            button2.Location = new Point(800, 71);
+            button2.BackColor = Color.SteelBlue;
+            button2.Cursor = Cursors.Hand;
+            button2.ForeColor = SystemColors.ControlLightLight;
+            button2.Location = new Point(900, 68);
             button2.Name = "button2";
-            button2.Size = new Size(152, 27);
+            button2.Size = new Size(171, 34);
             button2.TabIndex = 12;
             button2.Text = "Buscar";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
             button2.Click += button2_Click;
             // 
             // listView1
             // 
-            listView1.Location = new Point(489, 128);
+            listView1.Location = new Point(550, 121);
             listView1.MultiSelect = false;
             listView1.Name = "listView1";
-            listView1.Size = new Size(463, 206);
+            listView1.Size = new Size(520, 196);
             listView1.TabIndex = 13;
             listView1.UseCompatibleStateImageBehavior = false;
             listView1.ItemSelectionChanged += listView1_ItemSelectionChanged;
             // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1131, 28);
+            menuStrip1.TabIndex = 14;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(156, 24);
+            toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(9F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1005, 346);
+            BackColor = Color.GhostWhite;
+            ClientSize = new Size(1131, 328);
+            Controls.Add(menuStrip1);
             Controls.Add(listView1);
             Controls.Add(button2);
             Controls.Add(textBox5);
@@ -186,9 +222,13 @@
             Controls.Add(label2);
             Controls.Add(textBox1);
             Controls.Add(label1);
+            Font = new Font("Book Antiqua", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            MainMenuStrip = menuStrip1;
             Name = "Form1";
-            Text = "Form1";
+            Text = "Sistema";
             Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -209,5 +249,8 @@
         private TextBox textBox5;
         private Button button2;
         private ListView listView1;
+        private ContextMenuStrip contextMenuStrip1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem toolStripMenuItem1;
     }
 }
